5511196174b0caacd455b93e93582055d2ab15bb
gideongannaban <gideongannaban1985@gmail.com>
2020-12-28 22:01:34 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I3SpUDXo4W4DpSYg4AgnPcsXHNUoB5AC7xugVmEDXAjtg3xKRs9KTGxffyRCXzuVJYQAtwgVpprdoGIAcnXpaeW00ycFLD45x',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
