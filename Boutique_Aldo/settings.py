f2f76307cfe92215fc5c026f88f6ffcd0b48c406
gideongannaban <gideongannaban1985@gmail.com>
2021-01-05 23:26:04 +0000

    Finalizing deployment to Heroku

--- Boutique_Aldo/settings.py
+++ Boutique_Aldo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2n@d#w+6^9)17m^4(8g6nuwexj(l_t*&(d@=mgp&!#7i+*7=m('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-aldo.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'Boutique_Aldo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASES' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Boutique_aldo.wsgi:application
